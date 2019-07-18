a7837bd59c3e8615283a16218e8bf69bd7ab8615
Marcus Behel <marcus.behel@mindspring.com>
2019-07-17 23:49:26 -0400

    Wireless init script resets services as well

--- wirelessinit.sh
+++ wirelessinit.sh
@@ -4,4 +4,5 @@ ifdown --force wlan0 && ifdown --force ap0 && ifup ap0 && ifup wlan0
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 192.168.10.0/24 ! -d 192.168.10.0/24 -j MASQUERADE
 systemctl restart dnsmasq
-
+systemctl restart hostapd
+systemctl restart wpa_supplicant
