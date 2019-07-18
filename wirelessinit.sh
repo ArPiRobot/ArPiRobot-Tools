67241eaff39f1ab7191194b0103d2e2089c970a6
Marcus Behel <marcus.behel@mindspring.com>
2019-07-18 00:06:22 -0400

    wireless init no longer a restart script

--- wirelessinit.sh
+++ wirelessinit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-systemctl restart hostapd
-systemctl restart wpa_supplicant
 ifdown --force wlan0 && ifdown --force ap0 && ifup ap0 && ifup wlan0
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 192.168.10.0/24 ! -d 192.168.10.0/24 -j MASQUERADE
