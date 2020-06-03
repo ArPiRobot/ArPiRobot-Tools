a04cdb19234d6d4eb024778ffa1d3858f0b3b4a1
Marcus Behel <marcus.behel@mindspring.com>
2020-06-02 21:12:00 -0400

    Modified de-update_program to remove main.txt

--- helper-scripts/dt-update_program.sh
+++ helper-scripts/dt-update_program.sh
@@ -2,15 +2,13 @@
 
 # Usage: dt-update_program.sh full/path/to/project/folder relative/path/to/main/script.py
 
-if [ $# -ne 2 ]; then
-    printf "Usage: $0 project_folder main_script_relative_to_proj_folder\n"
+if [ $# -ne 1 ]; then
+    printf "Usage: $0 project_folder\n"
     exit 1
 fi
 
 PROJ_FOLDER="$1"
-MAIN_SCRIPT="$2"
 
 rm -rf /home/pi/arpirobot/*
 mkdir -p /home/pi/arpirobot/
 cp -r "$PROJ_FOLDER"/* ~/arpirobot/
-printf "$2\n" >> /home/pi/arpirobot/main.txt
