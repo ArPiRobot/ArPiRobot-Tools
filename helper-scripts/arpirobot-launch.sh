4a600bb37b64fa95ded53aa15737bb42e0d03350
Marcus Behel <marcus.behel@mindspring.com>
2020-06-02 20:58:07 -0400

    Program launch script will use a main.sh file if available instead of the old-style main.txt file

--- helper-scripts/arpirobot-launch.sh
+++ helper-scripts/arpirobot-launch.sh
@@ -1,6 +1,16 @@
-#!/bin/bash                                                                     
+#!/bin/bash
 
-file=$(head -n 1 ~/arpirobot/main.txt)
-PYTHONPATH=~/arpirobot python3 -u ~/arpirobot/$file > /tmp/arpirobot_program.log 2>&1
+MAIN_SCRIPT=~/arpirobot/main.sh
+MAIN_TXT=~/arpirobot/main.txt
+
+if [ -f "$MAIN_SCRIPT" ]; then
+    chmod +x "$MAIN_SCRIPT"
+    "$MAIN_SCRIPT" > /tmp/arpirobot_program.log 2>&1
+else
+    # Fallback to old method if no main.sh script
+    # main.txt would have a single line with the name of a python script to invoke
+    file=$(head -n 1 $MAIN_TXT)
+    PYTHONPATH=~/arpirobot python3 -u ~/arpirobot/$file > /tmp/arpirobot_program.log 2>&1
+fi
 
 exit 0
