fa536b98b2fc86506d1e0ca206a9542d9fdc3d8d
Marcus Behel <marcus.behel@mindspring.com>
2021-01-13 12:14:59 -0500

    Versions only reports image, python, tools

--- helper-scripts/dt-getversions.sh
+++ helper-scripts/dt-getversions.sh
@@ -29,15 +29,12 @@
 #              Output must match following example format
 #              IMAGE_VERSION_NAME
 #              PYTHON VERSION STRING from python3 --version
-#              PYLIB VERSION NAME
 #              RASPBIAN TOOLS VERSION
-#              UPDATE_VERSION_NAME
 #
 #              Example:
 #              Beta2
 #              3.7.1
 #              0.0.5
-#              0.0.3
 # author:      Marcus Behel
 # version:     v1.0.0
 #####################################################################################
@@ -57,15 +54,15 @@ python3 --version | sed -z 's/Python //g'
 # python3 -m pip show arpirobot-pythonlib | grep Version:
 # This will use the latest version found. Should only be one, but just in case.
 # cat `python3 -c "import arpirobot as a;print(a.__path__[0])"`/../ArPiRobot_PythonLib-*.dist-info/METADATA | awk '$1 ~ /^Version:/' | sed -z 's/Version: //g'
-
-ARRAY=( `python3 -c "import arpirobot as a;print(a.__path__[0])"`/../ArPiRobot_PythonLib-*.dist-info/METADATA )
-SELECTED_DIR=${ARRAY[-1]}
-if [ -f "$SELECTED_DIR" ]; then
-    PYLIB_VER=$(cat $SELECTED_DIR | awk '$1 ~ /^Version:/' | sed -z 's/Version: //g')
-else
-    PYLIB_VER="UNKNOWN"
-fi
-echo $PYLIB_VER
+#
+# ARRAY=( `python3 -c "import arpirobot as a;print(a.__path__[0])"`/../ArPiRobot_PythonLib-*.dist-info/METADATA )
+# SELECTED_DIR=${ARRAY[-1]}
+# if [ -f "$SELECTED_DIR" ]; then
+#     PYLIB_VER=$(cat $SELECTED_DIR | awk '$1 ~ /^Version:/' | sed -z 's/Version: //g')
+# else
+#     PYLIB_VER="UNKNOWN"
+# fi
+# echo $PYLIB_VER
 
 # Print raspbian tools version (no newline)
 VERSION=$(head -n 1 /usr/local/arpirobot-tools-version.txt 2> /dev/null | sed -z '$ s/\n$//')
@@ -75,8 +72,8 @@ fi
 printf "$VERSION\n"
 
 # Print update version
-VERSION=$(head -n 1 /usr/local/arpirobot-update-version.txt 2> /dev/null | sed -z '$ s/\n$//')
-if [ -z "$VERSION" ]; then
-    VERSION="UNKNOWN"
-fi
-printf "$VERSION\n"
+# VERSION=$(head -n 1 /usr/local/arpirobot-update-version.txt 2> /dev/null | sed -z '$ s/\n$//')
+# if [ -z "$VERSION" ]; then
+#     VERSION="UNKNOWN"
+# fi
+# printf "$VERSION\n"
